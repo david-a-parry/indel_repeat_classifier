1dc783e9f93c482134b5af2dc1ad23edf97bff49
David A. Parry <david.parry@ed.ac.uk>
2021-05-04 09:57:21 +0100

    allow relative import

--- indel_repeat_classifier/__init__.py
+++ indel_repeat_classifier/__init__.py
@@ -1,1 +1,4 @@
-from indel_repeat_classifier.version import __version__
+try:
+    from indel_repeat_classifier.version import __version__
+except ModuleNotFoundError:
+    from .version import __version__
