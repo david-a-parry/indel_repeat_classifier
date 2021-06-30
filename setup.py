3d3bf5366b9520109df689be4a481f8d205672a9
David A. Parry <david.parry@igmm.ed.ac.uk>
2021-06-30 10:00:34 +0100

    add scripts and requirements

--- setup.py
+++ setup.py
@@ -15,14 +15,20 @@ else:
 setup(
     name="indel_repeat_classifier",
     packages=["indel_repeat_classifier"],
+    scripts=[
+        "bin/short_repeats_from_fasta", "bin/filter_repeat_csv_by_regions"
+    ],
     version=verstr,
     description="Module for classifying repeat context of indels from VCFs.",
     author="David A. Parry",
     author_email="david.parry@ed.ac.uk",
     url='https://git.ecdf.ed.ac.uk/RER_deletions_paper/indel_repeat_classifier',
-    download_url='https://git.ecdf.ed.ac.uk/RER_deletions_paper/indel_repeat_classifier/archive/{}.tar.gz'.format(verstr),
+    download_url=
+    'https://git.ecdf.ed.ac.uk/RER_deletions_paper/indel_repeat_classifier/archive/{}.tar.gz'
+    .format(verstr),
     license='MIT',
-    install_requires=[],
+    install_requires=['pyfaidx'],
+    tests_require=['nose2', 'pysam'],
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -31,5 +37,5 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
-        ],
+    ],
 )
